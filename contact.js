751ac30f0c2eb5b2b460eb688ab31682615a111d
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2023-11-24 14:07:40 +0100

    contact

---
+++ contact.js
@@ -0,0 +1,34 @@
+const inputs = document.querySelectorAll("input");
+const textarea = document.querySelector("textarea");
+
+const submit = document.querySelector("button");
+
+const details = {
+    firstName: inputs[0].value,
+    lastName: inputs[1].value,
+    email: inputs[2].value,
+    message: textarea.value
+}
+
+if(document.title === "Contact"){
+    links[2].style.color = 'white'
+} else{
+    links[2].style.color = 'black'
+}
+
+submit.addEventListener("click", function(){
+    inputs.forEach((item)=>{
+        if(!item.value.trim() || !textarea.value.trim()){
+            return
+        }
+    })
+
+    let mailtoLink = "mailto:opebiyibiodun10@gmail.com?subject=suggestion&body=" + encodeURIComponent(`Name: ${details.lastName} ${details.firstName}
+    
+    \n
+    ${details.message}
+    `);
+
+            // Open the default email client
+            window.location.href = mailtoLink;
+})
